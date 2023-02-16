7e5a99c9d4f627cb94173331dcc1be3f1143e71b
Ernesto Neto <ernestoborges1995@gmail.com>
2023-02-16 11:13:42 -0300

    style: styled list style buttons from timers tab

--- src/components/TimerTab/TimerTab.jsx
+++ src/components/TimerTab/TimerTab.jsx
@@ -1,20 +1,29 @@
 import { TimerList } from "../TimerList/TimerList"
-import {useState} from "react";
+import { useState } from "react";
+import "./styles.css"
+import {IoGridOutline, IoListOutline} from "react-icons/io5"
+
 
 export function TimerTab({ selectedTab }) {
 
   const [listStyle, setListStyle] = useState("gallery")
 
-  function handleListStyle(type){
-    if(type !== listStyle){
+  function handleListStyle(type) {
+    if (type !== listStyle) {
       setListStyle(type)
     }
   }
 
   return (
     <section className={`timers-tab ${selectedTab !== "timers" ? "hide-tab" : ""}`}>
-      <button onClick={()=>handleListStyle("gallery")}>gallery</button>
-      <button onClick={()=>handleListStyle("list")}>list</button>
+      <div className="list-style-buttons-container">
+        <button className={listStyle === "gallery" ? "selected-style-btn" : ""} onClick={() => handleListStyle("gallery")}>
+          <IoGridOutline />
+        </button>
+        <button className={listStyle === "list" ? "selected-style-btn" : ""}  onClick={() => handleListStyle("list")}>
+          <IoListOutline />
+        </button>
+      </div>
       <TimerList listStyle={listStyle} />
     </section>
   );
